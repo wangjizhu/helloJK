d62788ca7015ecb11e09f7c0c67f3e56b1444512
Jude <jizhuw@hcsci.com>
2020-10-31 16:12:03 +0800

    基本实现

--- models/resourcemanager.go
+++ models/resourcemanager.go
@@ -42,16 +42,20 @@ func init(){
 
 	fmt.Println(_r.resources)
 
-	err = StartThread(1)
-	if err != nil {
-		panic(err)
-	}
+
+	//一秒一次轮询用于list
+	go func() {
+		for{
+			_ = _r.PollingList()
+			time.Sleep(1000 * time.Millisecond)
+		}
+	}()
 
 
 
 }
 
-//资源描述 申请和响应都用同样结构 根据实际情况使用不同字段
+//资源描述符 申请和响应都用同样结构 根据实际情况使用不同字段
 type ResourceDescriptor struct {
 	//查询是返回状态
 	Status int
@@ -74,7 +78,6 @@ type applyItem struct{
 }
 
 
-
 type ResourceManager struct {
 	//这个锁用来保证map多线程查询和更改时候的安全性 官方解决方案
 	sync.RWMutex
@@ -88,7 +91,7 @@ type ResourceManager struct {
 	applyingList *list.List
 
 	//已经被借走的资源列表 谁借了什么
-	offeredList *list.List
+	offeredMap map[string] [] ResourceDescriptor
 
 }
 
@@ -98,7 +101,7 @@ func NewResourceManager()*ResourceManager{
 		RWMutex:      sync.RWMutex{},
 		resources:    nil,
 		applyingList: nil,
-		offeredList:  nil,
+		offeredMap:  nil,
 	}
 }
 
@@ -113,8 +116,12 @@ func GetResourceManager()(*ResourceManager,error){
 //根据配置 初始化资源管理器 主要是置其中各种资源
 func (r *ResourceManager)Init()error{
 
-	r.resources=make(map[string] ResourceSet)
+	//初始化两个list
+	r.applyingList=list.New()
 
+
+	r.resources=make(map[string] ResourceSet)
+	r.offeredMap=make(map[string] [] ResourceDescriptor)
 
 	for i:=4;i<=11;i++{
 		//挂资源名
@@ -153,9 +160,14 @@ func (r *ResourceManager) registerResource(resourcename string,recourceamount in
 
 
 
-//向资源管理器申请某些资源 当全部都可以被使用时候返回 否则阻塞 可能会超时
+//向资源管理器申请某些资源 可以一次申请多个 当全部都可以被使用时候返回 否则阻塞 可能会超时
 func (r *ResourceManager)ApplyResource(username string ,resources ResourceSet) (ResourceSet,error){
-	fmt.Println("提出申请",username,resources)
+	//不需要申请 就不要瞎参和
+	if len(resources)==0{
+		return nil,errors.New("empty resource apply")
+	}
+
+
 
 	var getresourceset ResourceSet
 	//测试 一旦申请 直接满足回复 用于单线程测试
@@ -164,6 +176,8 @@ func (r *ResourceManager)ApplyResource(username string ,resources ResourceSet) (
 		applyResourceSet: resources,
 		resultChan:       make(chan ResourceSet),
 	}
+
+	fmt.Println("提出申请",applyitem)
 
 	//注册到申请列表
 	r.listLock.Lock()
@@ -177,7 +191,7 @@ func (r *ResourceManager)ApplyResource(username string ,resources ResourceSet) (
 			fmt.Println("成功获得资源",username,resources)
 			return getresourceset,nil
 
-		case <-time.After(100 * time.Second):
+		case <-time.After(20 * time.Second):
 			fmt.Println("申请等待超时!",username,resources)
 			return nil,errors.New("timeout")
 	}
@@ -185,34 +199,80 @@ func (r *ResourceManager)ApplyResource(username string ,resources ResourceSet) (
 }
 
 
-//归还资源
+//归还资源 强调 归还的资源需要携带id!!!
 func (r *ResourceManager)ReturnResource(username string ,resources ResourceSet)error{
 	fmt.Println("开始归还",username,resources)
 
+	for i:=0;i<len(resources);i++{
+		resourcename:=resources[i].ResourceName
+		id:=resources[i].ResourceId
+
+		//资源管理器中还回去
+		r.resources[resourcename][id].Enable=true
+		//offerdMap中去掉
+		err:=r.removeFromOfferdMap(username,resourcename,id)
+		if err != nil {
+			return err
+		}
+	}
+
+	fmt.Println("已归还",username,resources)
+	return nil
+}
+
+func (r *ResourceManager)removeFromOfferdMap(username string,resourcename string,id int)error{
+	for i:=0;i<len(r.offeredMap[username]);i++{
+		//找到后删除
+		if r.offeredMap[username][i].ResourceName==resourcename && r.offeredMap[username][i].ResourceId==id{
+			r.offeredMap[username]=append(r.offeredMap[username][:i], r.offeredMap[username][i+1:]...)
+		}
+	}
 	return nil
 }
 
 //核心!通过查找applyList 查找是否能够满足用户需求 并返回用户申请 ,,,每秒调用一次
 //若是多资源申请 要全部资源都可用情况下 才为可用 并返回全部资源到对应applyItem的resultChan里面
-func (r *ResourceManager)CheckList()error {
+func (r *ResourceManager)PollingList()error {
+
+	//fmt.Println("进入polling...")
 	var next *list.Element
 	for e := r.applyingList.Front(); e != nil; e = next {
 		// do something with e.Value 天坑这里 先把next存下来 万一删除了就找不到下一个了
 		next = e.Next()
-
+		applyitem:=e.Value.(applyItem)
+		fmt.Println(applyitem)
 		//检查这个节点里面的资源是否全部可用
-		available,err:= r.allResourcesAvailable(e.Value.(applyItem))
+		available,checkresult,err:= r.checkAllResourcesAvailableFromOneApplyItem(applyitem)
 		if err != nil {
 			return err
 		}
 
 		//全部资源可用
 		if available==true{
+			//去资源管理器记录下来
+			err:=r.getAllResourcesAvailableFromResult(checkresult)
+			if err != nil {
+				return err
+			}
+
+			//申请已经得到满足 从申请列表中去除
+			r.applyingList.Remove(e)
+
+			//表示申请成功 挂入offeredMap 记录下被谁取走
+			for i:=0;i<len(checkresult.applyResourceSet);i++{
+
+				r.offeredMap[applyitem.userName]=append(r.offeredMap[applyitem.userName], checkresult.applyResourceSet[i])
+
+			}
 
 
+			fmt.Println(checkresult.userName,"获得资源",checkresult.applyResourceSet)
+
+			//结果用通道返回
+			e.Value.(applyItem).resultChan <- checkresult.applyResourceSet
 
 
-		//	不是全部资源可用
+		//	不是全部资源可用 那就只有算求
 		}else{
 
 
@@ -225,16 +285,26 @@ func (r *ResourceManager)CheckList()error {
 	return nil
 }
 
-func (r *ResourceManager)allResourcesAvailable(applyitem applyItem)(bool,error){
+
+//检查节点内部的是否全部资源可用 返回是否可用,可用的资源,和错误
+func (r *ResourceManager)checkAllResourcesAvailableFromOneApplyItem(applyitem applyItem)(bool,applyItem,error){
 	//查询时锁住 其实没必要 因为一次只能一个查询
 	//r.queryLock.Lock()
 	allright:=true
 
 	for i:=0;i<len(applyitem.applyResourceSet);i++{
 		name:=applyitem.applyResourceSet[i].ResourceName
-		id:=applyitem.applyResourceSet[i].ResourceId
-		if r.resources[name][id].Enable==true{
 
+		//id是不知道的
+		affect,id,err:=r.checkResourceEnableFromResourceMap(name)
+		if err != nil {
+			return false,applyitem,err
+		}
+
+		//找到了资源
+		if affect==true{
+			//id记录下来
+			applyitem.applyResourceSet[i].ResourceId=id
 
 		}else{
 
@@ -243,17 +313,16 @@ func (r *ResourceManager)allResourcesAvailable(applyitem applyItem)(bool,error){
 
 		}
 
-
 	}
 
 	//都有
 	if allright==true{
 
-		return true,nil
-	//	有一个没有
+		return true,applyitem,nil
+	//至少有一个没有
 	}else{
 
-		return false,nil
+		return false,applyitem,nil
 
 	}
 
@@ -262,5 +331,34 @@ func (r *ResourceManager)allResourcesAvailable(applyitem applyItem)(bool,error){
 	//查完了 放开
 	//r.queryLock.Unlock()
 
+}
 
+//检查并返回具体资源可用的那个Id 没找到则返回false
+func (r *ResourceManager) checkResourceEnableFromResourceMap(resourcename string)(bool,int,error){
+
+	for i:=0;i<len(r.resources[resourcename]);i++{
+
+		if r.resources[resourcename][i].Enable==true{
+			return	true, i, nil
+		}
+
+	}
+
+	//没有空闲资源
+	return false, 0, nil
+}
+
+
+
+
+
+//通过对资源管理器资源标记,来把资源分配给用户 在先调用可用判断后才能分配
+func (r *ResourceManager)getAllResourcesAvailableFromResult(checkresult applyItem)error{
+	for i:=0;i<len(checkresult.applyResourceSet);i++{
+		name:=checkresult.applyResourceSet[i].ResourceName
+		id:=checkresult.applyResourceSet[i].ResourceId
+		//登记处取下来
+		r.resources[name][id].Enable=false
+	}
+	return nil
 }
--- models/threadmanager.go
+++ models/threadmanager.go
@@ -1,10 +1,66 @@
 package models
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
 	"time"
 )
+
+
+const (
+	Thread_Status_Runing=iota
+	Thread_Status_Paused
+	Thread_Status_Stoped
+	Thread_Status_Ready
+
+)
+
+//线程描述符
+type threadDescriptor struct{
+	threadName string
+	threadResources []ResourceDescriptor
+	threadStatus int
+}
+
+func NewThreadDescriptor(threadname string)*threadDescriptor{
+	return &threadDescriptor{
+		threadName:      threadname,
+		threadResources: nil,
+		threadStatus:    0,
+	}
+}
+
+func (th* threadDescriptor) addResource(resourceset ResourceSet) error{
+	th.threadResources = append(th.threadResources, resourceset...)
+	return nil
+}
+func (th* threadDescriptor) removeResource(resourceset ResourceSet) error{
+	//查找到对应资源并充线程中删除
+	for _,re:=range resourceset{
+
+		for index,value:=range th.threadResources{
+			if value.ResourceName==re.ResourceName && value.ResourceId==re.ResourceId{
+				th.threadResources = append(th.threadResources[:index],th.threadResources[index+1:]... )
+			}
+		}
+
+	}
+
+	return nil
+}
+
+func (th* threadDescriptor) getIdbyNameFromThread(resourcename string) (int,error){
+	for _,value:=range th.threadResources{
+		if value.ResourceName==resourcename {
+			return value.ResourceId,nil
+		}
+	}
+	return -1,errors.New("no such resourcename in this thread")
+}
+
+var  _threadTable map[string] *threadDescriptor
+
 
 //资源这一步是否需要申请和释放
 type resourceApplyAndReturn struct {
@@ -13,18 +69,45 @@ type resourceApplyAndReturn struct {
 	returnN bool
 }
 
+func init(){
+	_threadTable=make(map[string] *threadDescriptor)
+
+	fmt.Println("threadmanager 初始化完成")
+
+	err := StartThread("111")
+	if err != nil {
+		panic(err)
+	}
+}
+
+
+
+
+
+
 //type resourceRecord []resourceApplyAndReturn
 
 //在任何时候,开始一条线程,并开始执行
 //调用一次开启一个线程往下走
 
-func StartThread(threadId int)error{
+func StartThread(threadId string)error{
 
+	//线程启动 注册到threadmap
+	_threadTable[threadId]=NewThreadDescriptor(threadId)
+
+
+
+	var applyresult ResourceSet
+
+	resourcemanager,err:=GetResourceManager()
+	if err != nil {
+		return err
+	}
 
 
 	fmt.Println(len(rows))
 	for i:=4;i<len(rows);i++{
-		fmt.Println(i)
+		fmt.Println(rows[i][0])
 		//读入行资源
 		var resourceRecord []resourceApplyAndReturn
 		for j:=4;j<=11;j++{
@@ -51,11 +134,8 @@ func StartThread(threadId int)error{
 					rs.returnN=true
 				}
 
-
 			}
-
 			resourceRecord=append(resourceRecord, rs)
-
 		}
 
 		//读入行参数
@@ -66,10 +146,42 @@ func StartThread(threadId int)error{
 		}
 
 		//1.判断是否需要,并申请相应资源 申请apply为true的资源
+		applyresources:=ResourceSet{}
+		for k:=0;k<len(resourceRecord);k++{
+			//需要申请
+			if resourceRecord[k].apply==true{
+				applyresources = append(applyresources,
+					ResourceDescriptor{
+					Status:              0,
+					Enable:              false,
+					ResourceName:        resourceRecord[k].resourceName,
+					ResourceId:          0,
+					ResourceDescription: "",
+				})
+
+			}
+		}
 
 
 
+		//有需要申请的资源
+		if len(applyresources)>0{
+			fmt.Println("applyresources:",applyresources)
 
+			applyresult,err=resourcemanager.ApplyResource(threadId,applyresources)
+			if err != nil {
+				return err
+			}
+
+			//记录下来申请到的资源
+			err=_threadTable[threadId].addResource(applyresult)
+			if err != nil {
+				return err
+			}
+
+		}
+
+		fmt.Println("已经获得申请id",applyresult)
 
 		//2.允许通过后 开始模拟动作 等待时间后执行完成
 
@@ -77,32 +189,86 @@ func StartThread(threadId int)error{
 		if err != nil {
 			return err
 		}
-		executeAction(rows[i][1],rows[i][2],params,duration,resourceRecord)
+		executeAction(rows[i][1],rows[i][2],params,duration)
 
 
 
 		//3.执行完成后 根据策略 归还相应资源
+		returnresources:=ResourceSet{}
+		for k:=0;k<len(resourceRecord);k++{
+			//需要返回
+			if resourceRecord[k].returnN==true{
 
+				returnresourcedescriptor:=ResourceDescriptor{
+					Status:              0,
+					Enable:              false,
+					ResourceName:        resourceRecord[k].resourceName,
+					ResourceId:          0,
+					ResourceDescription: "",
+				}
 
+				//应该是从线程自己的资源里面选出来id
+				id,err:=_threadTable[threadId].getIdbyNameFromThread(resourceRecord[k].resourceName)
+				if err != nil {
+					return err
+				}
 
+				returnresourcedescriptor.ResourceId=id
+				returnresources = append(returnresources, returnresourcedescriptor)
+			}
+		}
+
+		if len(returnresources)>0{
+
+			fmt.Println("returnresources",returnresources)
+
+			err=resourcemanager.ReturnResource(threadId,returnresources)
+			if err != nil {
+				return err
+			}
+
+			//归还后 需要在线程记录中剔除次资源占用标记
+			_threadTable[threadId].removeResource(returnresources)
+
+		}
 
 
 	}
+
+	//从threadmap中去除此线程
+	delete(_threadTable,threadId)
+
+
+
+	fmt.Println("线程执行完毕!",threadId)
+	fmt.Println("目前线程资源管理器情况:",_threadTable[threadId].threadResources)
+	fmt.Println("目前资源管理器情况:",resourcemanager.applyingList,resourcemanager.offeredMap)
 
 
 	return nil
 }
 
+func getIdbyNameFromApplyResult(resourcename string,applyresult ResourceSet) (int,error){
+	for _,value := range applyresult{
+		if value.ResourceName==resourcename{
+			return value.ResourceId,nil
+		}
+
+	}
+
+	fmt.Println(resourcename,applyresult)
+	return -1,errors.New("no such resourcename")
+}
 
 
 //模拟动作执行
-func executeAction(actionName string,actionNameZH string,params interface{},duration int,resource []resourceApplyAndReturn){
+func executeAction(actionName string,actionNameZH string,params interface{},duration int){
 
 	defer timeCost(time.Now())
 	fmt.Println("动作开始:",actionName,actionNameZH,params)
-	fmt.Println(resource)
-	time.Sleep(time.Duration(duration/10) * time.Second)
-	fmt.Println("动作执行完毕!")
+
+	time.Sleep(time.Duration(duration/20) * time.Second)
+	fmt.Println("动作执行完毕!",actionName)
 }
 
 //耗时统计
